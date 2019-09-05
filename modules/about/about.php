f27f96ffc8efee03c2f9e65bb05bf96349568a4f
Andre Santos <uintdev@users.noreply.github.com>
2019-09-05 03:03:19 +0100

    Add GitHub link
    Aware of the one-liner issue. Will be worked on.

--- modules/about/about.php
+++ modules/about/about.php
@@ -9,6 +9,6 @@ if (!isset($con)) {
 if (!isset($verifier)) exiti('bad access'); // check not accessed via queryparser
 
 
-$info = 'msg.uint.dev - a messaging service<br>----------------------------<br>Build number: '.SITE_VERSION.'<br>Build type: '.SITE_BUILD;
+$info = 'msg.uint.dev - a messaging service<br>----------------------------<br>Build number: '.SITE_VERSION.'<br>Build type: '.SITE_BUILD.'<br><br>GitHub: <a href="https://github.com/uintdev/Msg" target="_blank" rel="noopener">https://github.com/uintdev/Msg</a>';
 
 echo $info;
